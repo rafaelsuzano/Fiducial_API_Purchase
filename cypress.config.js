13e589df4696649c30a57d1d467fdaf1ea60b030
Rafael Suzano <rafaelsuzano@hotmail.com>
2023-06-23 11:29:49 +0100

    ok

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,33 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        // `args` is an array of all the arguments that will
+        // be passed to browsers when it launches
+        console.log(launchOptions.args) // print all current args
+
+        if (browser.family === 'chromium' && browser.name !== 'electron') {
+          // auto open devtools
+          launchOptions.args.push('--auto-open-devtools-for-tabs')
+        }
+
+        if (browser.family === 'firefox') {
+          // auto open devtools
+          launchOptions.args.push('-devtools')
+        }
+
+        if (browser.name === 'electron') {
+          // auto open devtools
+          launchOptions.preferences.devTools = true
+        }
+
+        // whatever you return here becomes the launchOptions
+        return launchOptions
+      })
     },
   },
-});
+})
