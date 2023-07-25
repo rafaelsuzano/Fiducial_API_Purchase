957cd70bdf54315c4e3f51237ecf402089c16ce4
lgsduran <lgsduran@yahoo.com.br>
2023-07-25 17:00:01 +0100

    impl

--- cypress/e2e/web/e2e/login.cy.js
+++ cypress/e2e/web/e2e/login.cy.js
@@ -1,23 +1,30 @@
 const { default: menuItem } = require("../supplier/menuItems");
 const { default: supplier } = require("../supplier/createSupplier");
+const { default: contact } = require("../supplier/createContact");
+const { default: family } = require("../supplier/createFamily");
+
 
 describe("testing automation", () => {
   context("Given that test seed generated", () => {
     before(() => {
       cy.fixture("web/credentials").as("credential");
       cy.fixture("web/supplierInfo").as("sup");
+      cy.wrap('Fournisseur_'+Cypress._.random(0, 1e6)).as('name');      
     });
 
     context("And user logins successfully", () => {
       before(function () {
         cy.login(this.credential.credentialAdmin);
+        cy.loginSuccessfully();
       });
 
-      context("When validate Supplier functionalities", () => {
-        it("should create supplier", function () {
-          menuItem.openServices();
+      context("When validating Supplier functionalities", () => {
+        it("should create a full supplier", function () {
+          menuItem.goToAchat();
           menuItem.menuItem("fournisseur").click();
-          supplier.createGenericSupplier("Liechtenstein", this.sup.properties);
+          supplier.createGenericSupplier("Liechtenstein", this.name, this.sup.properties);
+          contact.fillContact();
+          family.fillFamily(Cypress._.random(0, 1e6));
           cy.contains('Sauvegarder').click();
         });
       });
