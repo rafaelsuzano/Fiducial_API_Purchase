50820fd61ee4956ed8cc883f496c0f3fe2782be6
Rafael Suzano <rafaelsuzano@hotmail.com>
2023-08-24 13:07:49 +0100

    ajuste sei la

--- integration_Ventes_Clients.sh
+++ integration_Ventes_Clients.sh
@@ -1,5 +1,5 @@
 echo 'Gera Relatorio Ventes Clients'
 
 #npx cypress run  --spec cypress/e2e/Ventes/Clients.cy.js  --config video=false,screenshotOnRunFailure=false --reporter junit --reporter-options "mochaFile=reports/TEST-[hash].xml"
-#junitparser merge  --glob "reports/TEST-*"  "reports/junit-report_Ventes_Clients.xml"
+junitparser merge  --glob "reports/TEST-*"  "reports/junit-report_Ventes_Clients.xml"
 trcli -y -h https://facilia.testrail.io/  --project "Facilia" --username  rafael.cruz-exterieur@fiducial.net  --password Suz@no30  parse_junit --title "Cypress API Automated Test Run Ventes_Clients" -f "./reports/junit-report_Ventes_Clients.xml"
