6c1c90fea003ca52ba697b5faa48cbca9b528960
Rafael Suzano <rafaelsuzano@hotmail.com>
2023-08-24 13:53:30 +0100

    novos sh

--- integration_Ventes_Countries.sh
+++ integration_Ventes_Countries.sh
@@ -1,5 +1,5 @@
 echo 'Gera Relatorio Ventes Countries'
-rm -rf cypress/reports/*.*
+rm -rf reports/*.*
 npx cypress run  --spec cypress/e2e/Ventes/Countries.cy.js  --config video=false,screenshotOnRunFailure=false --reporter junit --reporter-options "mochaFile=reports/TEST-[hash].xml"
 junitparser merge  --glob "reports/TEST-*"  "reports/junit-report_Ventes_Countries.xml"
 trcli -y -h https://facilia.testrail.io/  --project "Facilia" --username  rafael.cruz-exterieur@fiducial.net  --password Suz@no30  parse_junit --title "Cypress API Automated Test Run Ventes_Countries" -f "./reports/junit-report_Ventes_Countries.xml"
