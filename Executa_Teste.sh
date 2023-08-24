a64623b33eaddbb727d3e8ace909f882b301b9c0
Rafael Suzano <rafaelsuzano@hotmail.com>
2023-08-24 13:57:56 +0100

    novos sh

--- Executa_Teste.sh
+++ Executa_Teste.sh
@@ -1,19 +1,7 @@
 
 rm -rf reports/*.*
 ./integration_Ventes_Countries.sh 
-rm -rf reports/*.*
 ./integration_Ventes_Clients.sh
-rm -rf reports/*.*
 ./integration_DownloadImportModel.sh
 
 
-rm -rf reports/junit-report_Ventes_DownloadImportModel.xml
-rm -rf reports/junit-report_Ventes_Countries.xml
-rm -rf reports/junit-report_Ventes_Clients.xml
-
-
-
-
-junitparser merge  --glob "reports/TEST-*"  "reports/junit-report.xml"
-trcli -y -h https://facilia.testrail.io/  --project "Facilia" --username  rafael.cruz-exterieur@fiducial.net  --password Suz@no30  parse_junit --title "Cypress API Automated Test Run Ventes" -f "./reports/junit-report.xml"
-rm -rf reports/{,.[!.],..?}*
