9075d7fd53a64c40ee8b450d8f20a2e83fef1cbd
Rafael Suzano <rafaelsuzano@hotmail.com>
2023-08-28 11:55:00 +0100

    novo teste ]

--- Executa_Teste.sh
+++ Executa_Teste.sh
@@ -6,5 +6,13 @@ rm -rf reports/*.*
 rm -rf reports/*.*
 ./integration_DownloadImportModel.sh
 rm -rf reports/*.*
-./integration_InvoicesLastPaid.cy
+./integration_InvoicesLastPaid.sh
+rm -rf reports/*.*
+./integration_Ventes_Winsis_Settings.sh
+rm -rf reports/*.*
+./integration_Ventes_ValueRange.sh
+rm -rf reports/*.*
+./integration_Ventes_TaskManagement.sh
+rm -rf reports/*.*
+./integration_Management_Users.sh
 rm -rf reports/*.*
