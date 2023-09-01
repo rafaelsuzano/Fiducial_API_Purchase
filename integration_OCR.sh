9ff5ff002462baf579df18a3af40a0f3a1c9b1ba
Rafael Suzano <rafaelsuzano@hotmail.com>
2023-09-01 14:18:48 +0100

    novo teste ocr

--- integration_OCR.sh
+++ integration_OCR.sh
@@ -1,5 +1,5 @@
 echo 'Gera Relatorio ORC'
 rm -rf reports/*.*
-npx cypress run  --spec cypress/e2e/ORC/ORC.cy.js  --config video=false,screenshotOnRunFailure=false --reporter junit --reporter-options "mochaFile=reports/TEST-[hash].xml"
+npx cypress run  --spec cypress/e2e/OCR/OCR.cy.js  --config video=false,screenshotOnRunFailure=false --reporter junit --reporter-options "mochaFile=reports/TEST-[hash].xml"
 junitparser merge  --glob "reports/TEST-*"  "reports/junit-report_OCR.xml"
 trcli -y -h https://facilia.testrail.io/  --project "Facilia" --username  rafael.cruz-exterieur@fiducial.net  --password Suz@no30  parse_junit --title "Cypress API Automated Test Run OCR" -f "./reports/junit-report_OCR.xml"
