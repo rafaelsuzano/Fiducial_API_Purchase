902aaab0b0790f38e514b138290fb88d6ea3b5f1
Rafael Suzano <rafaelsuzano@hotmail.com>
2023-09-11 21:15:55 +0100

    novos cenarios ocr

--- cypress/e2e/OCR/OCR.cy.js
+++ cypress/e2e/OCR/OCR.cy.js
@@ -5,17 +5,8 @@ const addContext = require('mochawesome/addContext');
 
 let tt
 let dt1
-let id_delete
-let ID_TESTE
+let documentReferenceId
 
-let id_suppliers
-let dt_Family
-let id_suppliers_france
-let id_suppliers_Family
-let id_suppliers_contacts
-let id_suppliers_paymentInfo
-
-let Company = Cypress.env("companyId")
 
 describe('OCR', () => {
   before(() => {
@@ -25,8 +16,8 @@ describe('OCR', () => {
 
 
 
-      cy.api("POST", Cypress.env('Login') , {
-        "email": Cypress.env('email') ,
+      cy.api("POST", Cypress.env('Login'), {
+        "email": Cypress.env('email'),
         "password": Cypress.env('password')
       }).then((response) => {
 
@@ -38,115 +29,583 @@ describe('OCR', () => {
   })
 
   context('Get Status', () => {
-  it('Get documents references list TO_VALIDATE', () => {
-    cy.GET_API('ocr/documents-references?status=TO_VALIDATE', tt)
-      .then(Response => {
-        expect(Response.status).to.eq(200)
-
-
-       
-        cy.log( (JSON.stringify(Response.body)))
+    it('Get documents references list TO_VALIDATE', () => {
+      cy.GET_API('ocr/documents-references?status=TO_VALIDATE', tt)
+        .then(Response => {
+          expect(Response.status).to.eq(200)
 
 
 
-      })
+          cy.log((JSON.stringify(Response.body)))
+
+
+
+        })
+    })
+
+
+
+    it('Get documents references list NOT_PROCESSED', () => {
+      cy.GET_API('ocr/documents-references?status=NOT_PROCESSED', tt)
+        .then(Response => {
+          expect(Response.status).to.eq(200)
+
+          cy.log((JSON.stringify(Response.body)))
+
+
+        })
+    })
+
+
+
+
+
+    it('Get documents references list IN_PROCESS', () => {
+      cy.GET_API('ocr/documents-references?status=IN_PROCESS', tt)
+        .then(Response => {
+          expect(Response.status).to.eq(200)
+
+
+          cy.log((JSON.stringify(Response.body)))
+
+
+        })
+    })
+
+
+
+    it('Get documents references list PROCESSED', () => {
+      cy.GET_API('ocr/documents-references?status=PROCESSED', tt)
+        .then(Response => {
+          expect(Response.status).to.eq(200)
+
+
+
+          cy.log((JSON.stringify(Response.body)))
+
+
+
+
+        })
+    })
+
+
+    it('Get documents references list REFUSED', () => {
+      cy.GET_API('ocr/documents-references?status=REFUSED', tt)
+        .then(Response => {
+          expect(Response.status).to.eq(200)
+
+
+          cy.log((JSON.stringify(Response.body)))
+
+
+
+        })
+    })
+
+
+    it('Get documents references list ACCEPTED', () => {
+      cy.GET_API('ocr/documents-references?status=ACCEPTED', tt)
+        .then(Response => {
+          expect(Response.status).to.eq(200)
+
+
+          cy.log((JSON.stringify(Response.body)))
+
+
+
+        })
+    })
+
+
+    it('Get the nex document to be process', () => {
+      cy.GET_API('ocr/documents/next_to_process', tt)
+        .then(Response => {
+          expect(Response.status).to.eq(200)
+
+
+          cy.log((JSON.stringify(Response.body)))
+
+
+
+        })
+    })
+
+
   })
 
 
 
-  it('Get documents references list NOT_PROCESSED', () => {
-    cy.GET_API('ocr/documents-references?status=NOT_PROCESSED', tt)
-      .then(Response => {
-        expect(Response.status).to.eq(200)
-
-        cy.log( (JSON.stringify(Response.body)))
+  context(' e2e OCR', () => {
+    it('Create a new Document', () => {
 
 
+      cy.api({
+        method: "POST",
+        url: Cypress.env('url_achats') + 'ocr/new-document',
+        body: {
+          "companyId": Cypress.env('companyId'),
+          "base64": Cypress.env('Pbody_Base64'),
+
+          "fileName": "teste.pdf"
+        },
+
+
+        failOnStatusCode: false
+      }).then(Response => {
+
+        expect(Response.status).to.eq(201)
+        documentReferenceId = ((Response.body.documentReferenceId))
+        documentReferenceId = Response.body.documentReferenceId
+
+        cy.log(documentReferenceId)
       })
-  })
+
+
+    })
+    it('Get a document', () => {
+      cy.GET_API('ocr/document/' + documentReferenceId, tt)
+        .then(Response => {
+          expect(Response.status).to.eq(200)
+
+
+          cy.log((JSON.stringify(Response.body)))
 
 
 
+        })
+    })
+
+    it('Save OCR processed document with metadata', () => {
 
 
-  it('Get documents references list IN_PROCESS', () => {
-    cy.GET_API('ocr/documents-references?status=IN_PROCESS', tt)
-      .then(Response => {
-        expect(Response.status).to.eq(200)
+      cy.api({
+        method: "POST",
+        url: Cypress.env('url_achats') + 'ocr/documents',
+        body: {
 
 
-        cy.log( (JSON.stringify(Response.body)))
+          "documentReferenceId": documentReferenceId,
+
+          "base64": Cypress.env('Pbody_Base64'),
+
+          "fileName": "teste.pdf"
+        },
 
 
+        failOnStatusCode: false
+      }).then(Response => {
+
+        expect(Response.status).to.eq(201)
+
+        documentReferenceId = ((Response.body.documentReferenceId))
+        documentReferenceId = Response.body.documentReferenceId
+
+        cy.log(documentReferenceId)
       })
-  })
 
 
-
-  it('Get documents references list PROCESSED', () => {
-    cy.GET_API('ocr/documents-references?status=PROCESSED', tt)
-      .then(Response => {
-        expect(Response.status).to.eq(200)
+    })
 
 
-
-        cy.log( (JSON.stringify(Response.body)))
-
+    it('Accept a document ', () => {
 
 
+      cy.api({
+        method: "POST",
+        url: Cypress.env('url_achats') + 'ocr/document/' + documentReferenceId + '/accept',
+        body: {
 
+
+          "documentReferenceId": documentReferenceId,
+
+          "base64": Cypress.env('Pbody_Base64'),
+
+          "fileName": "teste.pdf"
+        },
+
+        headers: {
+          'Authorization': tt
+        },
+
+
+        failOnStatusCode: false
+      }).then(Response => {
+
+        expect(Response.status).to.eq(201)
+
+        documentReferenceId = ((Response.body.documentReferenceId))
+        documentReferenceId = Response.body.documentReferenceId
+
+        cy.log(documentReferenceId)
       })
-  })
 
 
-  it('Get documents references list REFUSED', () => {
-    cy.GET_API('ocr/documents-references?status=REFUSED', tt)
-      .then(Response => {
-        expect(Response.status).to.eq(200)
+    })
+
+    it('Refuse a document', () => {
 
 
-        cy.log( (JSON.stringify(Response.body)))
+      cy.api({
+        method: "POST",
+        url: Cypress.env('url_achats') + 'ocr/document/' + documentReferenceId + '/refuse',
+        body: {
 
 
+          "documentReferenceId": documentReferenceId,
 
+          "base64": Cypress.env('Pbody_Base64'),
+
+          "fileName": "teste.pdf"
+        },
+
+        headers: {
+          'Authorization': tt
+        },
+
+
+        failOnStatusCode: false
+      }).then(Response => {
+
+        expect(Response.status).to.eq(201)
+
+        documentReferenceId = ((Response.body.documentReferenceId))
+        documentReferenceId = Response.body.documentReferenceId
+
+        cy.log(documentReferenceId)
       })
-  })
 
 
-  it('Get documents references list ACCEPTED', () => {
-    cy.GET_API('ocr/documents-references?status=ACCEPTED', tt)
-      .then(Response => {
-        expect(Response.status).to.eq(200)
+    })
 
-     
-        cy.log( (JSON.stringify(Response.body)))
+    it('Update the document metadata_TO_VALIDATE', () => {
 
 
+      cy.api({
+        method: "PUT",
+        url: Cypress.env('url_achats') + 'ocr/document-metadata/' + documentReferenceId,
+        body: {
 
+
+
+          "documentReferenceId": documentReferenceId,
+          "status": "TO_VALIDATE",
+          "type": "INVOICE",
+          "siret": "string",
+          "supplierName": "string",
+          "mainCode": "string",
+          "documentDate": "2023-02-24T00:00:00Z",
+          "documentNumber": "string",
+          "currency": "EUR",
+          "amountTTC": 0,
+          "paymentTerms": [
+            {
+              "dueDate": "2023-02-24T00:00:00Z",
+              "amountTTC": 0,
+              "amountAlreadyPaid": 0,
+              "amountMissingPayment": 0,
+              "paymentDate": "2023-02-24T00:00:00Z"
+            }
+          ],
+          "comments": "TESTE AUTOMATICO"
+        },
+
+
+
+        headers: {
+          'Authorization': tt
+        },
+
+
+        failOnStatusCode: false
+      }).then(Response => {
+
+        expect(Response.status).to.eq(201)
+
+        documentReferenceId = ((Response.body.documentReferenceId))
+        documentReferenceId = Response.body.documentReferenceId
+
+        cy.log(documentReferenceId)
       })
-  })
 
 
-  it('Get the nex document to be process', () => {
-    cy.GET_API('ocr/documents/next_to_process', tt)
-      .then(Response => {
-        expect(Response.status).to.eq(200)
-
-     
-        cy.log( (JSON.stringify(Response.body)))
+    })
 
 
 
+    it('Update the document metadata_IN_PROCESS', () => {
+
+
+      cy.api({
+        method: "PUT",
+        url: Cypress.env('url_achats') + 'ocr/document-metadata/' + documentReferenceId,
+        body: {
+
+
+
+          "documentReferenceId": documentReferenceId,
+          "status": "IN_PROCESS",
+          "type": "INVOICE",
+          "siret": "string",
+          "supplierName": "string",
+          "mainCode": "string",
+          "documentDate": "2023-02-24T00:00:00Z",
+          "documentNumber": "string",
+          "currency": "EUR",
+          "amountTTC": 0,
+          "paymentTerms": [
+            {
+              "dueDate": "2023-02-24T00:00:00Z",
+              "amountTTC": 0,
+              "amountAlreadyPaid": 0,
+              "amountMissingPayment": 0,
+              "paymentDate": "2023-02-24T00:00:00Z"
+            }
+          ],
+          "comments": "TESTE AUTOMATICO"
+        },
+
+
+
+        headers: {
+          'Authorization': tt
+        },
+
+
+        failOnStatusCode: false
+      }).then(Response => {
+
+        expect(Response.status).to.eq(201)
+
+        documentReferenceId = ((Response.body.documentReferenceId))
+        documentReferenceId = Response.body.documentReferenceId
+
+        cy.log(documentReferenceId)
       })
-  })
 
+
+    })
+
+
+    it('Update the document metadata_PROCESSED', () => {
+
+
+      cy.api({
+        method: "PUT",
+        url: Cypress.env('url_achats') + 'ocr/document-metadata/' + documentReferenceId,
+        body: {
+
+
+
+          "documentReferenceId": documentReferenceId,
+          "status": "PROCESSED",
+          "type": "INVOICE",
+          "siret": "string",
+          "supplierName": "string",
+          "mainCode": "string",
+          "documentDate": "2023-02-24",
+          "documentNumber": "string",
+          "currency": "EUR",
+          "amountTTC": 0,
+          "paymentTerms": [
+            {
+              "dueDate": "2023-02-24",
+              "amountTTC": 0,
+              "amountAlreadyPaid": 0,
+              "amountMissingPayment": 0,
+              "paymentDate": "2023-02-24"
+            }
+          ],
+          "comments": "TESTE AUTOMATICO"
+        },
+
+
+
+        headers: {
+          'Authorization': tt
+        },
+
+
+        failOnStatusCode: false
+      }).then(Response => {
+
+        expect(Response.status).to.eq(201)
+
+        documentReferenceId = ((Response.body.documentReferenceId))
+        documentReferenceId = Response.body.documentReferenceId
+
+        cy.log(documentReferenceId)
+      })
+
+
+    })
+
+
+    it('Update the document metadata_REFUSED', () => {
+
+
+      cy.api({
+        method: "PUT",
+        url: Cypress.env('url_achats') + 'ocr/document-metadata/' + documentReferenceId,
+        body: {
+
+
+
+          "documentReferenceId": documentReferenceId,
+          "status": "REFUSED",
+          "type": "INVOICE",
+          "siret": "string",
+          "supplierName": "string",
+          "mainCode": "string",
+          "documentDate": "2023-02-24",
+          "documentNumber": "string",
+          "currency": "EUR",
+          "amountTTC": 0,
+          "paymentTerms": [
+            {
+              "dueDate": "2023-02-24",
+              "amountTTC": 0,
+              "amountAlreadyPaid": 0,
+              "amountMissingPayment": 0,
+              "paymentDate": "2023-02-24"
+            }
+          ],
+          "comments": "TESTE AUTOMATICO"
+        },
+
+
+
+        headers: {
+          'Authorization': tt
+        },
+
+
+        failOnStatusCode: false
+      }).then(Response => {
+
+        expect(Response.status).to.eq(201)
+
+        documentReferenceId = ((Response.body.documentReferenceId))
+        documentReferenceId = Response.body.documentReferenceId
+
+        cy.log(documentReferenceId)
+      })
+
+
+    })
+    it('Update the document metadata_ACCEPTED', () => {
+
+
+      cy.api({
+        method: "PUT",
+        url: Cypress.env('url_achats') + 'ocr/document-metadata/' + documentReferenceId,
+        body: {
+
+
+
+          "documentReferenceId": documentReferenceId,
+          "status": "ACCEPTED",
+          "type": "INVOICE",
+          "siret": "string",
+          "supplierName": "string",
+          "mainCode": "string",
+          "documentDate": "2023-02-24",
+          "documentNumber": "string",
+          "currency": "EUR",
+          "amountTTC": 0,
+          "paymentTerms": [
+            {
+              "dueDate": "2023-02-24",
+              "amountTTC": 0,
+              "amountAlreadyPaid": 0,
+              "amountMissingPayment": 0,
+              "paymentDate": "2023-02-24"
+            }
+          ],
+          "comments": "TESTE AUTOMATICO"
+        },
+
+
+
+        headers: {
+          'Authorization': tt
+        },
+
+
+        failOnStatusCode: false
+      }).then(Response => {
+
+        expect(Response.status).to.eq(201)
+
+        documentReferenceId = ((Response.body.documentReferenceId))
+        documentReferenceId = Response.body.documentReferenceId
+
+        cy.log(documentReferenceId)
+      })
+
+
+    })
+
+    it('Update the document metadata_NOT_PROCESSED', () => {
+
+
+      cy.api({
+        method: "PUT",
+        url: Cypress.env('url_achats') + 'ocr/document-metadata/' + documentReferenceId,
+        body: {
+
+
+
+          "documentReferenceId": documentReferenceId,
+          "status": "NOT_PROCESSED",
+          "type": "INVOICE",
+          "siret": "string",
+          "supplierName": "string",
+          "mainCode": "string",
+          "documentDate": "2023-02-24",
+          "documentNumber": "string",
+          "currency": "EUR",
+          "amountTTC": 0,
+          "paymentTerms": [
+            {
+              "dueDate": "2023-02-24",
+              "amountTTC": 0,
+              "amountAlreadyPaid": 0,
+              "amountMissingPayment": 0,
+              "paymentDate": "2023-02-24"
+            }
+          ],
+          "comments": "TESTE AUTOMATICO"
+        },
+
+
+
+        headers: {
+          'Authorization': tt
+        },
+
+
+        failOnStatusCode: false
+      }).then(Response => {
+
+        expect(Response.status).to.eq(201)
+
+        documentReferenceId = ((Response.body.documentReferenceId))
+        documentReferenceId = Response.body.documentReferenceId
+
+        cy.log(documentReferenceId)
+      })
+
+
+    })
+
+
+
+  })
 
 })
 
 
 
-context(' Post', () => {
 
-})
-
-})
+//http://127.0.0.1:4010/ocr/document/{documentReferenceId}/accept
