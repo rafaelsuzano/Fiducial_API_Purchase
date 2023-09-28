1998a62f9c80b07c1746f8f44bb061bcd6abc778
Rafael Suzano <rafaelsuzano@hotmail.com>
2023-09-28 18:43:47 -0300

    ajuste

--- cypress/e2e/Achats/PurchasesSirenDataBase.cy.js
+++ cypress/e2e/Achats/PurchasesSirenDataBase.cy.js
@@ -2,21 +2,14 @@ const { beforeEach } = require('mocha');
 const addContext = require('mochawesome/addContext');
 
 let dt1
-let dt
-let type
 let tt
 let id_suppliers_france
-let documentReferenceId
-let documentReferenceId_AVOIR
 let Company = Cypress.env("companyId")
 
 describe('OCR', () => {
   before(() => {
     cy.fixture('supplier.json').then(dt => {
       dt1 = dt;
-
-
-
 
       cy.api("POST", Cypress.env('Login'), {
         "email": Cypress.env('email'),
@@ -33,7 +26,7 @@ describe('OCR', () => {
 
 
 
-  it.skip('Post Supplier 201 France', () => {
+  it.skip('Post Supplier 201 France Sir', () => {
 
     cy.Post_API_With_Body('purchases/companies/' + Company + '/suppliers', tt, dt1['code201_France_SIRET_OCR'])
       .then(Response => {
