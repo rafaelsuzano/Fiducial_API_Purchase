857b3f65563cd58ceaa6ee12744701a0ab896fa3
Rafael Suzano <rafaelsuzano@hotmail.com>
2023-10-02 20:04:42 +0100

    novo teste

--- cypress/e2e/Achats/SupplierFamiliesMetrics.cy.js
+++ cypress/e2e/Achats/SupplierFamiliesMetrics.cy.js
@@ -6,13 +6,13 @@ const addContext = require('mochawesome/addContext');
 let tt
 let dt1
 let dt
-let id_suppliers
+let id_suppliers_Family
 
 let Company = Cypress.env("companyId")
 
 describe('Supplier Families Metrics', () => {
   before(() => {
-    cy.fixture('supplier.json').then(dt => {
+    cy.fixture('SupplierFamily').then(dt => {
       dt1 = dt;
 
 
@@ -30,22 +30,24 @@ describe('Supplier Families Metrics', () => {
     })
   })
 
-  it('Post Supplier', () => {
+  it('Post Supplier Family 201', () => {
 
-    cy.Post_API_With_Body('purchases/companies/'+Company+'/suppliers', tt, dt1['code201'])
-      .then(Response => {
-        cy.log(JSON.stringify(Response.body))
-        expect(Response.status).to.eq(201);
+    cy.Post_API_With_Body('purchases/companies/' + Company + '/supplier-families', tt, dt1['code201'])
+        .then(Response => {
 
-        id_suppliers = ((Response.body["id"]))
-      })
+            expect(Response.status).to.eq(201);
+            cy.log(JSON.stringify(Response.body))
+            id_suppliers_Family= ((Response.body["id"]))
+            cy.log(id_suppliers_Family)
+       
+        })
 
-  })
+})
 
 
 
   it('Get total amount of invoices by exercise month', () => {
-    cy.GET_API( 'purchases/companies/'+ Company + '/supplier-families/metrics/invoices/exercise-amount-monthly?supplierId='+id_suppliers, tt)
+    cy.GET_API( 'purchases/companies/'+ Company + '/supplier-families/metrics/invoices/exercise-amount-monthly?supplierFamilyId=' + id_suppliers_Family, tt)
       .then(Response => {
         expect(Response.status).to.eq(200)
         //var d =(JSON.stringify(Response.body))
@@ -65,7 +67,7 @@ describe('Supplier Families Metrics', () => {
 
 
   it('Get total amount of invoices  exercise year', () => {
-    cy.GET_API( 'purchases/companies/'+ Company + '/suppliers/metrics/invoices/exercise-amount-yearly', tt)
+    cy.GET_API( 'purchases/companies/'+ Company + '/suppliers/metrics/invoices/exercise-amount-yearly?supplierFamilyId=' + id_suppliers_Family, tt)
       .then(Response => {
         expect(Response.status).to.eq(200)
         //var d =(JSON.stringify(Response.body))
@@ -79,7 +81,7 @@ describe('Supplier Families Metrics', () => {
 
 
   it('Get total amount of invoices family by exercise year', () => {
-    cy.GET_API( 'purchases/companies/'+ Company + '/supplier-families/metrics/invoices/exercise-amount-yearly', tt)
+    cy.GET_API( 'purchases/companies/'+ Company + '/supplier-families/metrics/invoices/exercise-amount-yearly?supplierFamilyId=' + id_suppliers_Family, tt)
       .then(Response => {
         expect(Response.status).to.eq(200)
         //var d =(JSON.stringify(Response.body))
@@ -93,7 +95,7 @@ describe('Supplier Families Metrics', () => {
 
  
       it('Get total amount of invoices family civil year(monthly)', () => {
-    cy.GET_API( 'purchases/companies/'+ Company + '/supplier-families/metrics/invoices/civil-year-amount-monthly', tt)
+    cy.GET_API( 'purchases/companies/'+ Company + '/supplier-families/metrics/invoices/civil-year-amount-monthly?supplierFamilyId=' + id_suppliers_Family, tt)
       .then(Response => {
         expect(Response.status).to.eq(200)
         //var d =(JSON.stringify(Response.body))
@@ -106,7 +108,7 @@ describe('Supplier Families Metrics', () => {
 
 
   it('Get total amount of invoices civil year (January 1st to December 31st)', () => {
-    cy.GET_API( 'purchases/companies/'+ Company + '/supplier-families/metrics/invoices/civil-year-amount-yearly', tt)
+    cy.GET_API( 'purchases/companies/'+ Company + '/supplier-families/metrics/invoices/civil-year-amount-yearly?supplierFamilyId=' + id_suppliers_Family, tt)
       .then(Response => {
         expect(Response.status).to.eq(200)
         //var d =(JSON.stringify(Response.body))
@@ -118,7 +120,7 @@ describe('Supplier Families Metrics', () => {
   })
 
   it('Get total amount of invoices for the last 12 months', () => {
-    cy.GET_API( 'purchases/companies/'+ Company + '/supplier-families/metrics/invoices/amount-monthly?supplierId='+id_suppliers, tt)
+    cy.GET_API( 'purchases/companies/'+ Company + '/supplier-families/metrics/invoices/amount-monthly?supplierFamilyId=' + id_suppliers_Family, tt)
       .then(Response => {
         expect(Response.status).to.eq(200)
         //var d =(JSON.stringify(Response.body))
@@ -128,13 +130,15 @@ describe('Supplier Families Metrics', () => {
       })
   })
 
-  it('Delete suppliers', () => {    
-    cy.DELETE_API('purchases/companies/'+Company+'/suppliers/' + id_suppliers, tt)
+  it('Delete suppliers families', () => {    
+    cy.DELETE_API('purchases/companies/'+ Company +'/supplier-families/'+id_suppliers_Family, tt)
 
 
-
-  })
+       
+        })
 
 })
+
+
 
 
